f754a5b2e4a3847efaa5a1bc96a66c22a594b773
Emiliano <edesejas@eagerworks.com>
2023-06-10 23:33:36 -0300

    feat: add clerk auth

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,9 +1,11 @@
-import { type AppType } from "next/app";
+import { ClerkProvider } from "@clerk/nextjs";
+import type { AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
+const MyApp: AppType = ({ Component, pageProps }) => 
+  <ClerkProvider {...pageProps}>
+    <Component {...pageProps} />
+  </ClerkProvider>
 
 export default api.withTRPC(MyApp);
