f4aa04f3f1140de4cce74aba1ea6ab3e69532689
Santiago Calvo <48108808+sncalvo@users.noreply.github.com>
2023-06-12 02:54:41 -0300

    Feat: Add computacion selection

--- scrapper/src/index.ts
+++ scrapper/src/index.ts
@@ -23,6 +23,27 @@ async function start() {
   await page.waitForSelector(techDropdownSelector);
   await page.click(techDropdownSelector);
 
+  await sleep(1000);
+  const fingSelector = "td ::-p-text(FING - FACULTAD DE INGENIERÍA)";
+  await page.waitForSelector(fingSelector);
+  await page.click(fingSelector);
+
+  await sleep(2000);
+  const compSelector = `tr[data-ri="36"] > td[role="gridcell"]`;
+  await page.waitForSelector(compSelector);
+  await page.click(compSelector);
+
+  const compActiveSelector = `td::-p-text(INGENIERIA EN COMPUTACION) ~ td::-p-text(Si) + td > a[title="Ver más datos"]`;
+  await page.waitForSelector(compActiveSelector);
+  await page.click(compActiveSelector);
+
+  // const basicSubjectsSelector = "span.ui-tree-toggler:has(~ span::-p-text(2436 - MATERIAS BASICAS - min: 80 créditos))";
+  // await sleep(1000);
+  const basicSubjectTextSelector = "span::-p-text(2436 - MATERIAS BASICAS - min: 80 créditos)";
+  // await page.waitForSelector(basicSubjectsSelector);
+  // await page.click(basicSubjectsSelector);
+  // const basicSubjectSpan = await page.waitForSelector(basicSubjectSpan);
+
   await sleep(5000);
 
   await browser.close();
