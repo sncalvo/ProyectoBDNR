1b728c44978daa50c2c2c071d964f1a1c04b1703
Santiago Calvo <48108808+sncalvo@users.noreply.github.com>
2023-06-13 12:04:23 -0300

    Add open dropdown for basic subjects

--- scrapper/src/index.ts
+++ scrapper/src/index.ts
@@ -1,7 +1,7 @@
-import puppeteer from "puppeteer";
+import puppeteer, { ElementHandle } from "puppeteer";
 import { BEDELIAS_URL } from "./constants";
 
-import { sleep } from "./utils";
+import { clickSelector, hoverSelector, sleep } from "./utils";
 
 async function start() {
   const browser = await puppeteer.launch({ headless: false });
@@ -10,39 +10,36 @@ async function start() {
 
   await page.goto(BEDELIAS_URL);
   await page.setViewport({ width: 1280, height: 720 });
-  const studyPlansDropdownSelector = "a ::-p-text(PLANES DE ESTUDIO)";
-  await page.waitForSelector(studyPlansDropdownSelector);
-  await page.hover(studyPlansDropdownSelector);
 
-  const requirementsSelector = "a ::-p-text(Planes de estudio / Previas)";
-  await page.waitForSelector(requirementsSelector);
-  await sleep(2000);
-  await page.click(requirementsSelector);
+  const studyPlansDropdownSelector = "a::-p-text(PLANES DE ESTUDIO)";
+  await hoverSelector(studyPlansDropdownSelector, page);
 
-  const techDropdownSelector = "h3 ::-p-text(TECNOLOGÍA Y CIENCIAS DE LA NATURALEZA)";
-  await page.waitForSelector(techDropdownSelector);
-  await page.click(techDropdownSelector);
+  const requirementsSelector = "a::-p-text(Planes de estudio / Previas)";
+  await clickSelector(requirementsSelector, page, 2000);
+
+  const techDropdownSelector = "h3::-p-text(TECNOLOGÍA Y CIENCIAS DE LA NATURALEZA)";
+  await clickSelector(techDropdownSelector, page);
 
   await sleep(1000);
-  const fingSelector = "td ::-p-text(FING - FACULTAD DE INGENIERÍA)";
-  await page.waitForSelector(fingSelector);
-  await page.click(fingSelector);
+  const fingSelector = "td::-p-text(FING - FACULTAD DE INGENIERÍA)";
+  await clickSelector(fingSelector, page);
 
   await sleep(2000);
   const compSelector = `tr[data-ri="36"] > td[role="gridcell"]`;
-  await page.waitForSelector(compSelector);
-  await page.click(compSelector);
+  await clickSelector(compSelector, page);
 
   const compActiveSelector = `td::-p-text(INGENIERIA EN COMPUTACION) ~ td::-p-text(Si) + td > a[title="Ver más datos"]`;
-  await page.waitForSelector(compActiveSelector);
-  await page.click(compActiveSelector);
+  await clickSelector(compActiveSelector, page);
 
-  // const basicSubjectsSelector = "span.ui-tree-toggler:has(~ span::-p-text(2436 - MATERIAS BASICAS - min: 80 créditos))";
-  // await sleep(1000);
-  const basicSubjectTextSelector = "span::-p-text(2436 - MATERIAS BASICAS - min: 80 créditos)";
-  // await page.waitForSelector(basicSubjectsSelector);
-  // await page.click(basicSubjectsSelector);
-  // const basicSubjectSpan = await page.waitForSelector(basicSubjectSpan);
+  // Search through XPath span that is sibling of MATERIAS BASICAS (the arrow that is clicked to dropdown the menu)
+  const basicSubjectXPathSelector = "//span[contains(text(), '2436 - MATERIAS BASICAS - min: 80 créditos')]/preceding-sibling::span";
+  // need to sleep because page is loading. Maybe we can wait for page to stop loading by checking elements?
+  await sleep(2000);
+  const result = await page.$x(basicSubjectXPathSelector);
+  // Type shananigans. Refactor into clickXPathSelector
+  (result[0] as ElementHandle<Element>).click();
+
+  // TODO: Click on group and continue
 
   await sleep(5000);
 
--- scrapper/src/utils.ts
+++ scrapper/src/utils.ts
@@ -1,5 +1,19 @@
+import type { Page } from "puppeteer";
+
 export function sleep(time: number) {
   return new Promise((resolve) => {
     setTimeout(resolve, time);
   });
+}
+
+export async function clickSelector(clickSelector: string, page: Page, time?: number) {
+  await page.waitForSelector(clickSelector);
+  time && await sleep(time);
+  await page.click(clickSelector);
+}
+
+export async function hoverSelector(hoverSelector: string, page: Page, time?: number) {
+  await page.waitForSelector(hoverSelector);
+  time && await sleep(time);
+  await page.hover(hoverSelector);
 }
