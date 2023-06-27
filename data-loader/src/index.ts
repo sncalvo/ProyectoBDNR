97f8deb46a1c29fd9fca42540148986335f882a7
Santiago Calvo <48108808+sncalvo@users.noreply.github.com>
2023-06-27 19:29:28 -0300

    Add group relations

--- data-loader/src/index.ts
+++ data-loader/src/index.ts
@@ -8,6 +8,7 @@ import { Obj, camelize } from './utils';
 import getDriver from './driver';
 
 const SUBJECTS_FILE_PATH = "./input/subjects.yml";
+const GROUPS_FILE_PATH = "./input/groups.yml";
 
 const subjectSchema = z.object({
   code: z.string(),
@@ -15,9 +16,17 @@ const subjectSchema = z.object({
   credits: z.number(),
   hasExam: z.boolean(),
   subjectGroup: z.string().nullable(),
-})
+});
 
 const subjectsSchema = z.array(subjectSchema);
+
+const groupSchema = z.object({
+  code: z.string(),
+  name: z.string(),
+  minCredits: z.number(),
+});
+
+const groupsSchema = z.array(groupSchema);
 
 function readSubjects() {
   const fileBuffer = readFileSync(SUBJECTS_FILE_PATH, 'utf8');
@@ -26,8 +35,16 @@ function readSubjects() {
   return subjectsSchema.parse(objects);
 }
 
+function readGroups() {
+  const fileBuffer = readFileSync(GROUPS_FILE_PATH, 'utf8');
+  const objects = Object.values(camelize(parse(fileBuffer) as Obj));
+
+  return groupsSchema.parse(objects);
+}
+
 async function main() {
   const subjects = readSubjects();
+  const groups = readGroups();
 
   const driver = getDriver().session();
 
@@ -48,6 +65,41 @@ async function main() {
     );
 
     console.log('Inserted ', { name, code }, '---', index + 1, ' / ', subjects.length);
+  }
+
+  console.log('Inserting ', subjects.length, ' groups');
+  for (const [index, group] of groups.entries()) { 
+    const { name, code, minCredits } = group;
+
+    await driver.run(
+      'CREATE (n: Grupo { name: $name, code: $code, minCredits: $minCredits })',
+      {
+        name,
+        code,
+        minCredits: neo4j.int(minCredits),
+      }
+    );
+
+    console.log('Inserted ', { name, code }, '---', index + 1, ' / ', groups.length);
+  }
+
+  console.log('Relating subjects to groups');
+  for (const subject of subjects) {
+    const { code, subjectGroup } = subject;
+
+    if (!code || !subjectGroup) {
+      continue;
+    }
+
+    await driver.run(
+      `MATCH
+        (subject:Materia {code: $code}),
+        (group:Grupo {code: $subjectGroup})
+      CREATE (subject)-[:PERTENECE]->(group)`,
+      { code, subjectGroup },
+    );
+
+    console.log('Related ', { code, subjectGroup });
   }
 
   await driver.close();
