226856a78dc460bc0452bb833d9b8a3788db6fa8
Santiago Calvo <48108808+sncalvo@users.noreply.github.com>
2023-06-29 20:15:38 -0300

    stuff

--- data-loader/src/index.ts
+++ data-loader/src/index.ts
@@ -2,7 +2,7 @@ import { parse } from 'yaml';
 import { readFileSync } from 'fs';
 
 import * as z from 'zod';
-import neo4j from 'neo4j-driver'
+import neo4j, { Session } from 'neo4j-driver'
 
 import { Obj, camelize } from './utils';
 import getDriver from './driver';
@@ -10,6 +10,18 @@ import getDriver from './driver';
 const SUBJECTS_FILE_PATH = "./input/subjects.yml";
 const GROUPS_FILE_PATH = "./input/groups.yml";
 const PREREQUISITES_FILE_PATH = "./input/redes.yml";
+
+function makeid(length: number) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
 
 const subjectSchema = z.object({
   code: z.string(),
@@ -36,7 +48,16 @@ const basePrerequisiteSchema = z.object({
   needs: z.enum(['exam', 'course', 'all', 'enrollment']).optional(),
   subjectCode: z.string().optional(),
   isExam: z.boolean().optional(),
-}); 
+  credits: z.number().optional(),
+  group: z.number().optional(),
+});
+
+const mapLogicalOperatorToType = {
+  and: 'all',
+  not: 'cant_have',
+  or: 'one_of',
+  at_least: 'at_least'
+} as const;
 
 type Prerequisite = z.infer<typeof basePrerequisiteSchema> & {
   operands?: Prerequisite[];
@@ -69,45 +90,60 @@ function readPrerequisites() {
   return prerequisitesSchema.parse(objects);
 }
 
-const mapLogicalOperatorToType = {
-  and: 'all',
-  not: 'cant_have',
-  or: 'one_of',
-  at_least: 'at_least'
-} as const;
-
 // Goes over current prerequisite and creates the structure in the database. In is a recursive function.
-function savePrerequisites(prerequisite: Prerequisite, prevId?: string, index?: number): string {
-  const { subjectCode, type, logicalOperator, needs, subjectNeededCode } = prerequisite;
+function savePrerequisites(prerequisite: Prerequisite, prevId?: string, index?: number): [string, string[]] {
+  const { subjectCode, type, logicalOperator, needs, subjectNeededCode, operands } = prerequisite;
 
   // Base prerequisite
   if (subjectCode && logicalOperator) {
-    const query: string = `
-      MATCH (subject: Subject { code: ${subjectCode} })
+    const operandsQueries = operands?.map((operand, index) => savePrerequisites(operand, "prerequisite", index)) ?? [];
+    const { create, matches } = operandsQueries.reduce(
+      (prev, [createQuery, matchQueries]) => {
+        const create = [...prev.create, createQuery];
+        const matches = matchQueries.length ? [...prev.matches, matchQueries.join(',\n')] : [...prev.matches];
+        return ({ create, matches });
+      },
+      { create: [] as string[], matches: [] as string[] }
+    );
+
+    console.log({ create, matches });
+
+    const query = `
+      MATCH
+        (subject: Subject { code: '${subjectCode}' })
+          ${matches.length ? ',' : ''}
+          ${matches.join(',') ?? ''}
       CREATE
-        (subject)-[:HAS { type: ${mapLogicalOperatorToType[logicalOperator]} }]->(prerequisite: Prerequisite)${ prerequisite.operands ? ',' : ''}
-        ${
-          prerequisite.operands?.map((operand, index) => 
-          `// ${index} sub group
-            ${savePrerequisites(operand, "prerequisite", index)}`).join(',\n')
-        }
+        (subject)-[:HAS { type: '${mapLogicalOperatorToType[logicalOperator]}' }]->(prerequisite: Prerequisite)${create.length ? ',' : ''}
+        ${create.join('\n')}
     `;
-    return query;
+    return [query, []];
   } else {
     switch (type) {
       case 'logical':
         const type = mapLogicalOperatorToType[logicalOperator ?? 'and'];
-        const prerequisiteName = `${prevId}_${type}${ index ? `_${index}` : ''}`;
-        return `
+        const prerequisiteName = `${prevId}_${type}${index ? `_${index}` : ''}`;
+        return [`
           (${prevId})-[:SATISFIES { type: '${type}' }]->(${prerequisiteName}:Prerequisite),
           ${
-            prerequisite.operands?.map((operand, index) => savePrerequisites(operand, `${prerequisiteName}`, index)).join(',\n')
+            operands?.map((operand, index) => savePrerequisites(operand, `${prerequisiteName}`, index)).join(',\n')
           }
-        `.trim();
+        `.trim(),
+          []
+        ];
       case 'subject':
-        return `(${prevId})-[:NEEDS { type: '${needs}' }]->(:Subject { code: '${subjectNeededCode}' })`;
+        const subjectVarName = `subject_${index ? `_${index}` : ''}${prevId}_${needs}_${subjectNeededCode}`;
+
+        return [`(${prevId})-[:NEEDS { type: '${needs}' }]->(${subjectVarName})`, [`(${subjectVarName}:Subject { code: '${subjectNeededCode}' })`]];
+      case 'credits':
+        if (prerequisite.group) {
+          const groupVarName = `credits_${index ? `_${index}` : ''}${prevId}_${needs}_${subjectNeededCode}`;
+          return [`(${prevId})-[:NEEDS { type: '${needs}', min: '${prerequisite.credits}' }]->(${groupVarName})`, [`(${groupVarName}:Group { code: '${prerequisite.group}' })`]];
+        } else {
+          return [`(${prevId})-[:NEEDS { type: '${needs}', min: '${prerequisite.credits}' }]->(:Prerequisite)`, []];
+        }
       default:
-        return 'NOT IMPLEMENTED'
+        throw 'NOT IMPLEMENTED';
     }
   }
   // if subject, only relate to subject and return
@@ -116,11 +152,9 @@ function savePrerequisites(prerequisite: Prerequisite, prevId?: string, index?: 
   // For each operand, call savePrerequisite with prevId id of subject just created
 }
 
-async function main() {
+async function main(driver: Session) {
   const subjects = readSubjects();
   const groups = readGroups();
-
-  const driver = getDriver().session();
 
   console.log('Inserting ', subjects.length, ' subjects');
 
@@ -128,7 +162,7 @@ async function main() {
     const { name, code, credits, hasExam, subjectGroup } = subject;
 
     await driver.run(
-      'CREATE (n: Materia { name: $name, code: $code, credits: $credits, hasExam: $hasExam, subjectGroup: $subjectGroup })',
+      'CREATE (n: Subject { name: $name, code: $code, credits: $credits, hasExam: $hasExam, subjectGroup: $subjectGroup })',
       {
         name,
         code,
@@ -146,7 +180,7 @@ async function main() {
     const { name, code, minCredits } = group;
 
     await driver.run(
-      'CREATE (n: Grupo { name: $name, code: $code, minCredits: $minCredits })',
+      'CREATE (n: Group { name: $name, code: $code, minCredits: $minCredits })',
       {
         name,
         code,
@@ -167,30 +201,38 @@ async function main() {
 
     await driver.run(
       `MATCH
-        (subject:Materia {code: $code}),
-        (group:Grupo {code: $subjectGroup})
-      CREATE (subject)-[:PERTENECE]->(group)`,
+        (subject:Subject {code: $code}),
+        (group:Group {code: $subjectGroup})
+      CREATE (subject)-[:BELONGS]->(group)`,
       { code, subjectGroup },
     );
 
     console.log('Related ', { code, subjectGroup });
   }
 
-  const prerequisites = readPrerequisites();
-
-  console.log(prerequisites);
-
-  await driver.close();
+  // const prerequisites = readPrerequisites();
 }
 
-const testPrerequisites = async () => {
-
+const testPrerequisites = async (driver: Session) => {
   const prerequisites = readPrerequisites();
 
-  prerequisites.forEach((prerequisite) => {
-    console.log(savePrerequisites(prerequisite));
-  });
+  for (const prerequisite of prerequisites) {
+    const query = savePrerequisites(prerequisite);
+    try {
+      await driver.run(query[0]);
+      console.log(prerequisite);
+    } catch (error) {
+      console.log(error, query, JSON.stringify(prerequisite));
+    }
+
+    // console.log('Inserted prerequisite ', prerequisite.subjectCode);
+  }
 } 
 
-testPrerequisites();
-// main();
+
+(async () => {
+  const driver = getDriver().session();
+
+  await main(driver);
+  await testPrerequisites(driver);
+})();
